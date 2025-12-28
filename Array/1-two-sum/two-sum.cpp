d5e510293283719c669f13db78957941bcc79f9d
prachalm28 <51396990+prachalm28@users.noreply.github.com>
2025-12-28 09:19:31 +0530

    Time: 0 ms (100.00%) | Memory: 14.9 MB (37.04%) - LeetSync

--- Array/1-two-sum/two-sum.cpp
+++ Array/1-two-sum/two-sum.cpp
@@ -17,3 +17,25 @@ public:
         return {};
     }
 };
+
+// #include <vector>
+
+// using namespace std;
+
+// class Solution {
+// public:
+//     vector<int> twoSum(vector<int>& nums, int target) {
+//         int n = nums.size();
+
+//         for (int i = 0; i < n; i++) {
+//             for (int j = i + 1; j < n; j++) {
+//                 if (nums[i] + nums[j] == target) {
+//                     return { i, j };
+//                 }
+//             }
+//         }
+
+//         // One solution is guaranteed
+//         return {};
+//     }
+// };
